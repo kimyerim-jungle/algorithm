8f27a57a1450de9fa388d9b42c6df49dd4f31964
Kim-YeRim <155847825+kimyerim-jungle@users.noreply.github.com>
2024-01-29 15:10:00 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -0,0 +1,32 @@
+import sys
+
+string = sys.stdin.readline()
+num = []
+n = ''
+add = 0
+for i in range(len(string)):
+    if string[i] == '+':
+        add += int(n)
+        n=''
+    elif string[i] == '-':
+        if add != 0: 
+            num.append(add + int(n))
+            add = 0
+        else: 
+            num.append(int(n))
+        n=''
+    else:
+        n += string[i]
+    
+    if i == len(string)-1:
+        add += int(n)
+        num.append(add)
+
+result = num[0]
+def dp(result):
+    for i in range(1, len(num)):
+        result = result - num[i]   
+    return result
+            
+result = dp(result)
+print(result)
